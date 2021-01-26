41e5eba4ea94e94bae65f732681d23b7205048e4
Zenia Gist <zeniagist@gmail.com>
2021-01-26 16:06:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+let operatorElement = document.querySelector('.operator');
+let operator = "+";
+function calculate(){
+  // define input numbers
+  let number1 = parseInt(document.querySelector('#input1').value);
+  let number2 = parseInt(document.querySelector('#input2').value);
+  let result = 0;
+
+  // 
+  switch(operator){
+    case "+":
+      result = add(number1, number2);
+      break;
+    case "-":
+      result = subtract(number1, number2);
+      break;
+    case "*":
+      result = multiply(number1, number2);
+      break;
+    case "/":
+      result = divide(number1, number2);
+      break;
+  }
+  document.querySelector('.output').innerText = result;
+}
+
+document.querySelector('#add').addEventListener('click', function(){
+  operator = "+";
+  operatorElement.value = operator;
+});
+
+document.querySelector('#sub').addEventListener('click', function(){
+  operator = "-";
+  operatorElement.value = operator;
+});
+
+document.querySelector('#mul').addEventListener('click', function(){
+  operator = "*";
+  operatorElement.value = operator;
+});
+
+document.querySelector('#div').addEventListener('click', function(){
+  operator = "/";
+  operatorElement.value = operator;
+});
+
+document.querySelector('#eql').addEventListener('click', function(){
+  calculate();
+});
+
+// functions
+function add(number1, number2){
+  return number1 + number2;
+}
+
+function subtract(number1, number2){
+  return number1 - number2;
+}
+
+function multiply(number1, number2){
+  return number1 * number2;
+}
+
+function divide(number1, number2){
+  return number1 / number2;
+}
